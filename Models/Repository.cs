915fed859a356b4c4595171cf99278a0bf7dd410
yurakushniruk <yurakushniruk@ukr.net>
2023-03-05 13:45:19 +0100

    Added Repository and GitHubClient classes
    Created a GitHubClient service to retrieve repositories of a user from GitHub. Created a new model Repository to be used by the GitHub service

---
+++ Models/Repository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace GitHubRepos.API.Models
+{
+    public class Repository
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string HtmlUrl { get; set; }
+    }
+}
+
---
+++ Services/GitHub/GitHubClient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace GitHubRepos
+{
+    public class GitHubClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _baseUrl;
+        private readonly string _userAgent;
+
+        public GitHubClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _baseUrl = Configuration.GetSection("GitHub:BaseUrl").Value;
+            _userAgent = Configuration.GetSection("GitHub:UserAgent").Value;
+        }
+
+        public async Task<List<Repository>> GetRepositoriesAsync(string username)
+        {
+            var url = $"{_baseUrl}users/{username}/repos";
+
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            request.Headers.UserAgent.Add(new ProductInfoHeaderValue(_userAgent, "1.0"));
+
+            var response = await _httpClient.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var repositories = JsonConvert.DeserializeObject<List<Repository>>(content);
+
+            return repositories;
+        }
+    }
+}
